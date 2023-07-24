35510c32ca21c5aa09efba083945e6c592cf181f
gregmika <grzegorz.mika@relia-sol.pl>
2023-07-24 23:26:04 +0200

    add test for entries

---
+++ internal/db/entries_test.go
@@ -0,0 +1,83 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"BankApplication/internal/util"
+
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomEntry(t *testing.T) (CreateEntryParams, Entry) {
+	arg := CreateEntryParams{
+		AccountID: util.RandomAccountID(),
+		Amount:    util.RandomMoney(),
+	}
+
+	// due to the foreign key constraint on accounts, we need to create the account first
+	query := `INSERT INTO accounts (id, owner, balance, currency) VALUES ($1, $2, $3, $4)`
+	_, err := testQueries.db.Exec(context.Background(), query, arg.AccountID,
+		util.RandomOwner(), util.RandomMoney(), util.RandomCurrency())
+	require.NoError(t, err)
+
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	return arg, entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		arg, entry := createRandomEntry(t)
+		require.NotEmpty(t, entry)
+		require.Equal(t, arg.AccountID, entry.AccountID)
+		require.NotZero(t, entry.CreatedAt)
+		require.Equal(t, arg.Amount, entry.Amount)
+		require.GreaterOrEqual(t, entry.Amount, float64(0))
+		require.NotZero(t, entry.ID)
+	}
+}
+
+func TestGetEntry(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		arg, createdEntry := createRandomEntry(t)
+		entry, err := testQueries.GetEntry(context.Background(), createdEntry.ID)
+		require.NoError(t, err)
+		require.NotEmpty(t, entry)
+		require.Equal(t, arg.AccountID, entry.AccountID)
+		require.NotZero(t, entry.CreatedAt)
+		require.Equal(t, arg.Amount, entry.Amount)
+		require.GreaterOrEqual(t, entry.Amount, float64(0))
+		require.NotZero(t, entry.ID)
+	}
+}
+
+func TestListEntries(t *testing.T) {
+	accountID := util.RandomAccountID()
+
+	// due to the foreign key constraint on accounts, we need to create the account first
+	query := `INSERT INTO accounts (id, owner, balance, currency) VALUES ($1, $2, $3, $4)`
+	_, err := testQueries.db.Exec(context.Background(), query, accountID,
+		util.RandomOwner(), util.RandomMoney(), util.RandomCurrency())
+	require.NoError(t, err)
+
+	for i := 0; i < 20; i++ {
+		arg := CreateEntryParams{
+			AccountID: accountID,
+			Amount:    util.RandomMoney(),
+		}
+		_, err := testQueries.CreateEntry(context.Background(), arg)
+		require.NoError(t, err)
+	}
+	arg := ListEntriesParams{
+		AccountID: accountID,
+		Limit:     5,
+		Offset:    5,
+	}
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+	}
+}
--- internal/util/random.go
+++ internal/util/random.go
@@ -25,7 +25,17 @@ func RandomFloat(min, max float64) float64 {
 	return min + rand.Float64()*(max-min)
 }
 
-// RandomString returns a random string of length length.
+// RandomInt returns a random integer between min and max.
+func RandomInt(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+// RandomPositiveInt returns a random non-negative integer
+func RandomPositiveInt(max int) int {
+	return RandomInt(0, max)
+}
+
+// RandomString returns a random string of length `length`.
 func RandomString(length int) string {
 	var sb strings.Builder
 	k := len(letters)
@@ -49,4 +59,9 @@ func RandomMoney() float64 {
 // RandomCurrency returns a random currency.
 func RandomCurrency() string {
 	return currencies[rand.Intn(len(currencies))]
+}
+
+// RandomAccountID returns a random account ID.
+func RandomAccountID() int64 {
+	return int64(RandomPositiveInt(100000000000))
 }
