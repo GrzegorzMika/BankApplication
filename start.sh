68e77e0d5a8b53ea4c9ec7d7d71f7a0dc2efe99f
gregmika <grzegorz.mika@relia-sol.pl>
2023-08-26 20:12:11 +0200

    fix

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "run db migration"
 . /app/app.env
-/app/soda migrate up -p /db -c /db/database.yml
+/app/soda migrate up -p /db -c /db/database.yml -e "production"
 
 echo "start the application"
 exec "$@"
