6fc6b5d96e64dc3a4c2a9f5534bc60dcdcf5ad84
gregmika <grzegorz.mika@relia-sol.pl>
2023-09-18 23:19:39 +0200

    add GRPC tests to protect paths

--- internal/gapi/mail_test.go
+++ internal/gapi/mail_test.go
@@ -1,10 +1,17 @@
 package gapi
 
 import (
+	"context"
+	"fmt"
+
 	"BankApplication/internal/db"
+	"BankApplication/internal/token"
 	"BankApplication/internal/util"
 	"BankApplication/internal/worker"
+
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/metadata"
+
 	"testing"
 	"time"
 )
@@ -17,4 +24,16 @@ func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDist
 	server, err := NewServer(config, store, taskDistributor)
 	require.NoError(t, err)
 	return server
+}
+
+func newContextWithBearerToken(t *testing.T, tokenMaker token.Maker, username string, duration time.Duration) context.Context {
+	accessToken, _, err := tokenMaker.CreateToken(username, duration)
+	require.NoError(t, err)
+	bearerToken := fmt.Sprintf("%s %s", authorizationBearer, accessToken)
+	md := metadata.MD{
+		authorizationHeader: []string{
+			bearerToken,
+		},
+	}
+	return metadata.NewIncomingContext(context.Background(), md)
 }
